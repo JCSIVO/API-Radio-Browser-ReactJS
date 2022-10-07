6dc96f673cec44ac6cff001103d0cb121f66f8a5
JCSIVO <josecsivo@gmail.com>
2022-10-07 17:04:32 +0200

    Api-Radio-Browser-ReactJS
    ReactJS Avanzado Open BootCamp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { IoPlay } from 'react-icons/io5';
 
 function App() {
+  const [busqueda, setBusqueda] = useState("");
+  const [listado, setListado] = useState([]);
+
+  const hazBusqueda = () => {
+    const url = `https://de1.api.radio-browser.info/json/stations/byname/${busqueda}`;
+    axios.get(url)
+      .then(r => setListado(r.data))
+      .catch(e => console.error(e))
+  } 
+
+  const playRadio = (radio) => {
+    const audio = new Audio(radio.url)
+    audio.play();
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Bienvenido a la aplicación <span>OpenRadioCamp</span></h1>
+    <input type="text" placeholder='Escribe el nombre de la radio' 
+    value={busqueda} onChange={e => setBusqueda(e.target.value)}></input>
+    <button onClick={hazBusqueda}>Buscar  </button>
+    {listado.length > 0 && <div aria-label='length-not-null'></div>}
+    <section aria-label='listado-emisoras'>
+    {listado.map((emisora, i) => <div key={i}>{emisora.name} <IoPlay 
+    style={{ cursor: 'pointer' }} onClick={() => playRadio(emisora)}/> </div>)}
+    </section>
     </div>
   );
 }
